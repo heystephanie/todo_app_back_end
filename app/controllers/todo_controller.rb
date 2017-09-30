2c5b888b0f1012e453b4159edb8d17bd1f3b7847
Stephanie <stephanie.ppena27@gmail.com>
2017-09-30 17:57:03 +0000

    Add Dynamic Route

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,11 +1,9 @@
 class TodoController <ApplicationController
     def idex
     end
+    
     def show 
+        @todo_description="Todo"
+        @todo_promodoro_estimate=4
     end
-    @todo_description="Todo"
-    @todo_promodoro_estimate=4
-    end
-    @todo_description
-    @todo_promodoro_estimate
 end
