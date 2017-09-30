b3b30b8a22e9e51945bc490a633f11ea51d62643
Stephanie <stephanie.ppena27@gmail.com>
2017-09-30 19:23:54 +0000

    If statement

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,16 @@
 class TodoController <ApplicationController
-    def idex
+    def index
     end
     
     def show 
-        @todo_description="Todo"
-        @todo_promodoro_estimate=4
+        todo_id='2'
+        
+        if todo_id=='1'
+            @todo_description="Todo"
+            @todo_promodoro_estimate=4
+        elsif todo_id=='2'
+            @todo_description="Todo list"
+            @todo_promodoro_estimate= 3
+        end
     end
 end
