b367f82ad20a3e98cffb96397f2343e2305ed3e6
Stephanie <stephanie.ppena27@gmail.com>
2017-09-30 17:16:05 +0000

    declare instance variable

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,11 @@
 class TodoController <ApplicationController
     def idex
     end
-
+    def show 
+    end
+    @todo_description="Todo"
+    @todo_promodoro_estimate=4
+    end
+    @todo_description
+    @todo_promodoro_estimate
 end
