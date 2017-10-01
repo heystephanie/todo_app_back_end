97b88e5ee30c30c26d26284d67b07100bfafa072
Stephanie <stephanie.ppena27@gmail.com>
2017-10-01 13:38:15 +0000

    Update statement

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,14 +3,29 @@ class TodoController <ApplicationController
     end
     
     def show 
-        todo_id='2'
+        todo_id=params [:id]
+        
         
         if todo_id=='1'
-            @todo_description="Todo"
+            @todo_description="Make the Curriculum"
             @todo_promodoro_estimate=4
         elsif todo_id=='2'
-            @todo_description="Todo list"
+            @todo_description=""
             @todo_promodoro_estimate= 3
+            
+        todo_id==3
+        @todo_description=""
+        @todo_promodoro_estimate=
+        
+        todo_id==4
+        @todo_description=""
+        @todo_promodoro_estimate=
+        todo_id==5
+        @todo_description=""
+        @todo_promodoro_estimate=
+        todo_id==6
+        @todo_description=""
+        @todo_promodoro_estimate=
         end
     end
 end
