ee0343c004e4fb4ef2fb89e1b1ca61d6fb1b0afb
Stephanie <stephanie.ppena27@gmail.com>
2017-10-01 16:33:37 +0000

    Add Datta to database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,24 +8,24 @@ class TodoController <ApplicationController
         
         if todo_id=='1'
             @todo_description="Make the Curriculum"
-            @todo_promodoro_estimate=4
+            @todo_promodoro_estimate=1
         elsif todo_id=='2'
-            @todo_description=""
-            @todo_promodoro_estimate= 3
+            @todo_description="Buy Supplies"
+            @todo_promodoro_estimate= 2
             
         elsif todo_id==3
-        @todo_description="Finish cleaning"
-        @todo_promodoro_estimate=6
-        
+             @todo_description="Clean The dishes"
+            @todo_promodoro_estimate=3
         elsif todo_id==4
-        @todo_description="Do homework"
-        @todo_promodoro_estimate=5
+            @todo_description="Do homework"
+            @todo_promodoro_estimate=4
         elsif todo_id==5
-        @todo_description="Buy food"
-        @todo_promodoro_estimate=4
+            @todo_description="Buy food"
+            @todo_promodoro_estimate=5
         elsif todo_id==6
-        @todo_description="Clean"
-        @todo_promodoro_estimate=3
+            @todo_description= "Do homework for history"
+            @todo_promodoro_estimate=6
+            
         end
     end
 end
